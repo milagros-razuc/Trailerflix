48583ffbcf0dc460d9f7466039260e7588d0b003
Milagros Razuc <126362614+milagros-razuc@users.noreply.github.com>
2025-07-24 18:41:46 -0300

    Update getCatalogo.js

--- src/routes/controllers/getCatalogo.js
+++ src/routes/controllers/getCatalogo.js
@@ -1,8 +1,8 @@
-const Catalogo = require('../../models/catalogo');
+const catalogo = require('../../models/catalogo');
 
 module.exports = async(req, res) =>{
     try{
-        const Catalogo = await Catalogo.findAll();
+        const Catalogo = await catalogo.findAll();
         res.status(200).json(Catalogo);
     }catch(error){
         res.status(500).json({error:'error al buscar el catalogo'});
